90120d114893534d46cc2f1d729f92194f3232da
AlexChow <alexchow@iMac.local>
2016-04-23 10:31:09 +0800

    add contact request access

--- Vessage/ConversationListContactCell.swift
+++ Vessage/ConversationListContactCell.swift
@@ -15,6 +15,41 @@ class ConversationListContactCell:ConversationListCellBase,ABPeoplePickerNavigat
     
     @IBOutlet weak var titleLabel: UILabel!
     override func onCellClicked() {
+        let authStatus = ABAddressBookGetAuthorizationStatus();
+        if authStatus == .NotDetermined {
+            
+            let action = UIAlertAction(title: "OK".localizedString(), style: .Default, handler: { (ac) in
+                if let addressBookRef = ABAddressBookCreateWithOptions(nil , nil){
+                    let addressBook = addressBookRef.takeRetainedValue()
+                    ABAddressBookRequestAccessWithCompletion(addressBook, { (granted, error) in
+                        dispatch_async(dispatch_get_main_queue(), {
+                            if error != nil{
+                                self.showNoPersimissionAlert()
+                            }else if granted {
+                                self.showContactView()
+                            }else{
+                                self.showNoPersimissionAlert()
+                            }
+                        })
+                    })
+                    
+                }else{
+                    self.showNoPersimissionAlert()
+                }
+            })
+            self.rootController.showAlert("REQUEST_ACCESS_CONTACT_TITLE".localizedString(), msg: "REQUEST_ACCESS_CONTACT".localizedString(), actions: [action])
+        }else if authStatus == .Authorized{
+            showContactView()
+        }else{
+            self.showNoPersimissionAlert()
+        }
+    }
+    
+    private func showNoPersimissionAlert(){
+        self.rootController.showAlert("NO_CONTACT_PERSIMISSION_TITLE".localizedString(), msg: "NO_CONTACT_PERSIMISSION".localizedString())
+    }
+    
+    private func showContactView(){
         let controller = ABPeoplePickerNavigationController()
         controller.peoplePickerDelegate = self
         self.rootController.presentViewController(controller, animated: true) { () -> Void in
